f5214b5b72f4e5ccccb974ca88cae2cf728864b0
ADImantra <C.S.Minty@Gmail.com>
2023-07-19 00:08:39 -0700

    basic operator loop is complete. Add, subtract, multiply, divide, exponent work properly

--- scripts.js
+++ scripts.js
@@ -2,15 +2,15 @@ let displayTextBase = [];
 let displayTextNew = [];
 let operatorCurrent = ``;
 let primed = false;
-
-let baseValue = parseInt(displayTextBase.join(), 10);
-let newValue = parseInt(displayTextNew.join(), 10);
+let baseValue = 0;
+let newValue = 0;
 
 const numpad = document.querySelectorAll(`.number`);
 const display = document.querySelector(`.num-display`);
 
 const clearButton = document.getElementById(`clear`);
 const multiplyButton = document.getElementById(`multiply`);
+const divideButton = document.getElementById(`divide`);
 const subtractButton = document.getElementById(`subtract`);
 const addButton = document.getElementById(`add`);
 const remainderButton = document.getElementById(`remainder`);
@@ -19,12 +19,13 @@ const exponentButton = document.getElementById(`exponent`);
 const result = document.getElementById(`result`);
 
 multiplyButton.addEventListener(`click`, () => {
-    if (!primed) {
-        operatorCurrent = `multiply`
-        primed = true;
-    } else {
-        //
-    }
+    operatorCurrent = `multiply`
+    primed = true;
+});
+
+divideButton.addEventListener(`click`, () => {
+    operatorCurrent = `divide`
+    primed = true;
 });
 
 subtractButton.addEventListener(`click`, () => {
@@ -52,18 +53,117 @@ exponentButton.addEventListener(`click`, () => {
     primed = true;
 });
 
-result.addEventListener(`click` ,() => {
-
-})
-
 numpad.forEach(num => num.addEventListener(`click`, () => {
-    displayTextBase.push(num.textContent)
-    display.textContent = displayTextBase.join(``); 
-}))
+    if (!primed) {
+        displayTextBase.push(num.textContent);
+        baseValue = parseInt(displayTextBase.join(``), 10)
+        display.textContent = displayTextBase.join(``);
+        console.log(baseValue)
+        console.log(newValue)
+        
+    } else {
+        displayTextNew.push(num.textContent);
+        newValue = parseInt(displayTextNew.join(``), 10)
+        display.textContent = displayTextNew.join(``);
+        console.log(baseValue)
+        console.log(newValue)
+    };
+
+}));
+
+function multiply(baseV, newV) {
+    return baseV * newV;
+};
+
+function divide(baseV, newV) {
+    return baseV / newV;
+};
+
+function subtract(baseV, newV) {
+    return baseV - newV;
+};
+
+function add(baseV, newV) {
+    return baseV + newV;
+};
+
+function remainder(baseV, newV) {
+    return baseV % newV;
+};
+
+function sqrt(baseV, newV) {
+    return baseV + newV;
+};
+
+function exponent(baseV, newV) {
+    geoSum = 1;
+    for (let i = 0; i < newV; i++) {
+        geoSum = geoSum * baseV
+    }
+    return geoSum;
+};
+
+
+result.addEventListener(`click`, () => {
+    switch (operatorCurrent) {
+        case `multiply`:
+            baseValue = multiply(baseValue, newValue);
+            display.textContent = baseValue;
+            displayTextBase = [];
+            displayTextNew = [];
+            break
+
+        case `divide`:
+            baseValue = divide(baseValue, newValue);
+            display.textContent = baseValue;
+            displayTextBase = [];
+            displayTextNew = [];
+            break
+
+        case `subtract`:
+            baseValue = subtract(baseValue, newValue);
+            display.textContent = baseValue;
+            displayTextBase = [];
+            displayTextNew = [];
+            break
+
+        case `add`:
+            baseValue = add(baseValue, newValue);
+            display.textContent = baseValue;
+            displayTextBase = [];
+            displayTextNew = [];
+            break
+
+        case `remainder`:
+            baseValue = remainder(baseValue, newValue);
+            display.textContent = baseValue;
+            displayTextBase = [];
+            displayTextNew = [];
+            break
+
+        case `sqrt`:
+            baseValue = sqrt(baseValue);
+            display.textContent = baseValue;
+            displayTextBase = [];
+            displayTextNew = [];
+            break
+
+        case `exp`:
+            baseValue = exponent(baseValue, newValue);
+            display.textContent = baseValue;
+            displayTextBase = [];
+            displayTextNew = [];
+            break
+    }
+});
 
 clearButton.addEventListener(`click`, () => {
     displayTextBase = [];
-    display.textContent = 0;
+    displayTextNew = [];
+    baseValue = 0;
+    newValue = 0;
+    operatorCurrent = ``;
     primed = false;
+    display.textContent = 0;
 });
 
